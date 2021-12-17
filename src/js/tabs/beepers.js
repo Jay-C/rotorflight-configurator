2c1e7c39d4458a255efb83a600e404ecfc9393ea
Petri Mattila <petri.t.j.mattila@gmail.com>
2021-12-17 10:16:18 +0000

    WIP: Auto hide toolbar on Beepers tab

--- src/js/tabs/beepers.js
+++ src/js/tabs/beepers.js
@@ -21,6 +21,21 @@ TABS.beepers.initialize = function (callback) {
 
     function process_html() {
 
+        // Hide the buttons toolbar
+        $('.tab-beepers').addClass('toolbar_hidden');
+
+        // translate to user-selected language
+        i18n.localizePage();
+
+        let toolbarHidden = true;
+
+        function showToolbar() {
+            if (toolbarHidden) {
+                toolbarHidden = false;
+                $('.tab-beepers').removeClass('toolbar_hidden');
+            }
+        }
+
         // Dshot Beeper
         const dshotBeeper_e = $('.tab-beepers .dshotbeeper');
         const dshotBeacon_e = $('.tab-beepers .dshotbeacon');
@@ -53,30 +68,27 @@ TABS.beepers.initialize = function (callback) {
 
         FC.BEEPER_CONFIG.beepers.generateElements(template, destination);
 
-        // translate to user-selected language
-        i18n.localizePage();
-
         $('input.condition', beeper_e).change(function () {
             const element = $(this);
             FC.BEEPER_CONFIG.beepers.updateData(element);
         });
 
+        $('.content_wrapper').change(function () {
+            showToolbar();
+        });
+
+        $('a.revert').click(function() {
+            self.refresh();
+        });
+
         $('a.save').click(function() {
-
-            function save_beeper_config() {
-                MSP.send_message(MSPCodes.MSP_SET_BEEPER_CONFIG, mspHelper.crunch(MSPCodes.MSP_SET_BEEPER_CONFIG), false, save_to_eeprom);
-            }
-
-            function save_to_eeprom() {
-                MSP.send_message(MSPCodes.MSP_EEPROM_WRITE, false, save_completed);
-            }
-
-            function save_completed() {
-                GUI.log(i18n.getMessage('beepersEepromSaved'));
-                TABS.beepers.initialize();
-            }
-
-            save_beeper_config();
+            Promise.resolve(true)
+                .then(() => MSP.promise(MSPCodes.MSP_SET_BEEPER_CONFIG, mspHelper.crunch(MSPCodes.MSP_SET_BEEPER_CONFIG)))
+                .then(() => MSP.promise(MSPCodes.MSP_EEPROM_WRITE))
+                .then(() => {
+                    GUI.log(i18n.getMessage('eepromSaved'));
+                    self.refresh();
+                });
         });
 
         GUI.content_ready(callback);
@@ -86,3 +98,13 @@ TABS.beepers.initialize = function (callback) {
 TABS.beepers.cleanup = function (callback) {
     if (callback) callback();
 };
+
+TABS.beepers.refresh = function (callback) {
+    const self = this;
+
+    GUI.tab_switch_cleanup(function () {
+        self.initialize();
+        if (callback) callback();
+    });
+};
+
